dbd3b984a73cf6846967bd2f835ba3d5ca09d145
Rémi Angot <remi.angot@gmail.com>
2023-01-21 12:59:40 +0100

    Update loadJson.test.js

--- tests/loadJson.test.js
+++ tests/loadJson.test.js
@@ -8,27 +8,14 @@ import Figure from 'src/Figure'
  */
 const save = {
   apiGeomVersion: 0.1,
-  element0: {
-    type: 'Point',
-    x: 0,
-    y: 0,
-    label: 'A',
-    shape: 'x',
-    size: 0.1,
-    id: 'element0',
-    color: 'black',
-    thickness: 1,
-    isDashed: false,
-    isVisible: true
-  },
   element1: {
     type: 'Point',
+    id: 'element1',
     x: 4,
     y: 1,
     label: 'B',
     shape: 'x',
     size: 0.1,
-    id: 'element1',
     color: 'blue',
     thickness: 1,
     isDashed: false,
@@ -36,13 +23,36 @@ const save = {
   },
   element2: {
     type: 'Point',
+    id: 'element2',
     x: 2,
     y: 4,
     label: 'C',
     shape: 'x',
     size: 0.1,
-    id: 'element2',
     color: 'blue',
+    thickness: 1,
+    isDashed: false,
+    isVisible: true
+  },
+  element4: {
+    type: 'VectorByPoints',
+    id: 'element4',
+    idPoint1: 'element1',
+    idPoint2: 'element2',
+    color: 'black',
+    thickness: 1,
+    isDashed: false,
+    isVisible: true
+  },
+  element0: {
+    type: 'Point',
+    id: 'element0',
+    x: 0,
+    y: 0,
+    label: 'M',
+    shape: 'x',
+    size: 0.1,
+    color: 'black',
     thickness: 1,
     isDashed: false,
     isVisible: true
@@ -59,60 +69,6 @@ const save = {
     thickness: 1,
     isDashed: false,
     isVisible: true
-  },
-  element4: {
-    type: 'VectorByPoints',
-    idPoint1: 'element1',
-    idPoint2: 'element2',
-    color: 'black',
-    thickness: 1,
-    isDashed: false,
-    isVisible: true
-  },
-  element5: {
-    type: 'LineByPointVector',
-    idPoint: 'element0',
-    idVector: 'element4',
-    id: 'element5',
-    color: 'black',
-    thickness: 1,
-    isDashed: false
-  },
-  element6: {
-    type: 'LinePerpendicular',
-    idLine: 'element3_segment0',
-    idPoint: 'element2',
-    id: 'element6',
-    color: 'black',
-    thickness: 1,
-    isDashed: false
-  },
-  element7: {
-    type: 'LineParallel',
-    idLine: 'element3_segment0',
-    idPoint: 'element2',
-    id: 'element7',
-    color: 'black',
-    thickness: 1,
-    isDashed: false
-  },
-  element8: {
-    type: 'LineParallel',
-    idLine: 'element3_segment1',
-    idPoint: 'element0',
-    id: 'element8',
-    color: 'black',
-    thickness: 1,
-    isDashed: false
-  },
-  element9: {
-    type: 'LineParallel',
-    idLine: 'element3_segment2',
-    idPoint: 'element1',
-    id: 'element9',
-    color: 'black',
-    thickness: 1,
-    isDashed: false
   }
 }
 
@@ -121,11 +77,11 @@ geo.loadJson(save)
 
 test('Chargement', () => {
   expect(save).toEqual(JSON.parse(geo.json))
-  // const A = geo.elements.get('element1')
-  // A.moveTo(1, 1)
-  // A.moveTo(0, 0)
-  // const C = geo.elements.get('element5')
-  // C.moveTo(6, 2)
-  // C.moveTo(3, 5)
+  const A = geo.elements.get('element1')
+  A.moveTo(1, 1)
+  A.moveTo(4, 1)
+  const C = geo.elements.get('element2')
+  C.moveTo(6, 2)
+  C.moveTo(2, 4)
   expect(save).toEqual(JSON.parse(geo.json))
 })
